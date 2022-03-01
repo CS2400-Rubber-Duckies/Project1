431a3f1b337f9fe526c0f6612e703ece0b8f7077
uhnguyen101 <70361768+uhnguyen101@users.noreply.github.com>
2022-02-28 20:50:53 -0800

    updated linkedbagtest

--- LinkedBagTest.java
+++ LinkedBagTest.java
@@ -1,8 +1,11 @@
+import java.util.*;
+
 public class LinkedBagTest {
 
     public static <T> void main(String[] args) {
         BagInterface<String> testBag1 = new LinkedBag<>();
         BagInterface<String> testBag2 = new LinkedBag<>();
+        BagInterface<String> testBag3 = new LinkedBag<>();
 
         // add method
         testBag1.add("t");
@@ -13,26 +16,25 @@ public class LinkedBagTest {
         testBag2.add("o");
 
         // print out contents in bag 1 and bag 2
-        System.out.println("TEST BAG1 CONTENT: " + testBag1);
-        System.out.println("TEST BAG2 CONTENT: " + testBag2);
+        System.out.println("TEST BAG1 CONTENT: " + Arrays.toString(testBag1.toArray()));
+        System.out.println("TEST BAG2 CONTENT: " + Arrays.toString(testBag2.toArray()));
         // getting current size
-        System.out.println("BAG1 CURRENT SIZE: " + testBag1.getCurrentSize());
+        System.out.println("BAG 1 CURRENT SIZE: " + testBag1.getCurrentSize());
 
         // checking if the bag isEmpty (false)
-        System.out.println("BAG1 IS EMPTY: " + testBag1.isEmpty());
+        System.out.println("BAG 1 IS EMPTY: " + testBag1.isEmpty());
 
         // remove method and isEmpty (true)
         testBag1.remove("w"); // remove specified entry
         testBag1.remove(); // remove unspecified entry
-        System.out.println("BAG1 IS EMPTY: " + testBag1.isEmpty());
+        System.out.println("BAG 1 IS EMPTY: " + testBag1.isEmpty());
 
         // readding elements to bag
         testBag1.add("a");
         testBag1.add("b");
 
         // getFrequencyOf method
-        testBag2.getFrequencyOf("o");
-        System.out.println(testBag2);
+        System.out.println("FREQUENCY OF \"o\": " + testBag2.getFrequencyOf("o") + "\n");
 
         // contains method (false)
         testBag1.contains("c");
@@ -40,28 +42,41 @@ public class LinkedBagTest {
         testBag1.contains("a");
 
         // INTERSECTION METHOD TESTING
-        System.out.println("---------- INTERSECTION METHOD TESTING ----------");
+        System.out.println("---------- INTERSECTION METHOD TESTING ----------" + "\n");
 
         // intersection method: (case 1) when they don't have same elements
-        System.out.println("Bag 1: " + testBag1 + "\n" + "Bag 2: " + testBag2);
+        System.out.println("Bag 1: " + Arrays.toString(testBag1.toArray()) + "\n" + "Bag 2: "
+                + Arrays.toString(testBag2.toArray()));
+        testBag3 = testBag1.intersection(testBag2);
         System.out.println(
-                "Intersection Case 1 - when they don't have any matching elements: " + testBag1.intersection(testBag2));
+                "Intersection Case 1 - when they don't have any matching elements: "
+                        + Arrays.toString(testBag3.toArray()) + "\n");
 
         // intersection method: (case 2) when they have same elements
         testBag1.add("o");
-        System.out.println("Bag 1: " + testBag1 + "\n" + "Bag 2: " + testBag2);
+        System.out.println("Bag 1: " + Arrays.toString(testBag1.toArray()) + "\n" + "Bag 2: "
+                + Arrays.toString(testBag2.toArray()));
+        testBag3 = testBag1.intersection(testBag2);
         System.out
-                .println("Intersection Case 2 - when they have matching elements: " + testBag1.intersection(testBag2));
+                .println("Intersection Case 2 - when they have matching elements: "
+                        + Arrays.toString(testBag3.toArray()) + "\n");
 
         // intersection method: (case 3) when one is null
         testBag1.clear();
-        System.out.println("Bag 1: " + testBag1 + "\n" + "Bag 2: " + testBag2);
-        System.out.println("Intersection Case 3 - when one of the bags are null: " + testBag1.intersection(testBag2));
+        System.out.println("Bag 1: " + Arrays.toString(testBag1.toArray()) + "\n" + "Bag 2: "
+                + Arrays.toString(testBag2.toArray()));
+        testBag3 = testBag1.intersection(testBag2);
+        System.out
+                .println("Intersection Case 3 - when one of the bags are null: " + Arrays.toString(testBag3.toArray())
+                        + "\n");
 
         // intersection method: (case 4) when both are null
         testBag2.clear();
-        System.out.println("Bag 1: " + testBag1 + "\n" + "Bag 2: " + testBag2);
-        System.out.println("Intersection Case 4 - when both bags are null: " + testBag1.intersection(testBag2));
+        System.out.println("Bag 1: " + Arrays.toString(testBag1.toArray()) + "\n" + "Bag 2: "
+                + Arrays.toString(testBag2.toArray()));
+        testBag3 = testBag1.intersection(testBag2);
+        System.out.println(
+                "Intersection Case 4 - when both bags are null: " + Arrays.toString(testBag3.toArray()) + "\n");
 
         // intersection method: (case 5) when all elements match
         testBag1.add("c");
@@ -74,32 +89,48 @@ public class LinkedBagTest {
         testBag2.add("e");
         testBag2.add("f");
 
-        System.out.println("Bag 1: " + testBag1 + "\n" + "Bag 2: " + testBag2);
-        System.out.println("Intersection Case 4 - when all elements match: " + testBag1.intersection(testBag2));
+        System.out.println("Bag 1: " + Arrays.toString(testBag1.toArray()) + "\n" + "Bag 2: "
+                + Arrays.toString(testBag2.toArray()));
+        testBag3 = testBag1.intersection(testBag2);
+        System.out.println(
+                "Intersection Case 4 - when all elements match: " + Arrays.toString(testBag3.toArray()) + "\n");
 
         // DIFFERENCE METHOD TESTING
-        System.out.println("---------- DIFFERENCE METHOD TESTING ----------");
+        System.out.println("---------- DIFFERENCE METHOD TESTING ----------" + "\n");
 
         // difference method: (case 1) when they don't have same elements
-        System.out.println("Bag 1: " + testBag1 + "\n" + "Bag 2: " + testBag2);
+        System.out.println("Bag 1: " + Arrays.toString(testBag1.toArray()) + "\n" + "Bag 2: "
+                + Arrays.toString(testBag2.toArray()));
+        testBag3 = testBag1.difference(testBag2);
         System.out.println(
-                "Difference Case 1 - when they don't have any matching elements: " + testBag1.difference(testBag2));
+                "Difference Case 1 - when they don't have any matching elements: "
+                        + Arrays.toString(testBag3.toArray()) + "\n");
 
         // difference method: (case 2) when they have same elements
         testBag1.add("o");
-        System.out.println("Bag 1: " + testBag1 + "\n" + "Bag 2: " + testBag2);
+        System.out.println("Bag 1: " + Arrays.toString(testBag1.toArray()) + "\n" + "Bag 2: "
+                + Arrays.toString(testBag2.toArray()));
+        testBag3 = testBag1.difference(testBag2);
         System.out
-                .println("Difference Case 2 - when they have matching elements: " + testBag1.difference(testBag2));
+                .println(
+                        "Difference Case 2 - when they have matching elements: " + Arrays.toString(testBag3.toArray())
+                                + "\n");
 
         // difference method: (case 3) when one is null
         testBag1.clear();
-        System.out.println("Bag 1: " + testBag1 + "\n" + "Bag 2: " + testBag2);
-        System.out.println("Difference Case 3 - when one of the bags are null: " + testBag1.difference(testBag2));
+        System.out.println("Bag 1: " + Arrays.toString(testBag1.toArray()) + "\n" + "Bag 2: "
+                + Arrays.toString(testBag2.toArray()));
+        testBag3 = testBag1.difference(testBag2);
+        System.out.println(
+                "Difference Case 3 - when one of the bags are null: " + Arrays.toString(testBag3.toArray()) + "\n");
 
         // difference method: (case 4) when both are null
         testBag2.clear();
-        System.out.println("Bag 1: " + testBag1 + "\n" + "Bag 2: " + testBag2);
-        System.out.println("Difference Case 4 - when both bags are null: " + testBag1.difference(testBag2));
+        System.out.println("Bag 1: " + Arrays.toString(testBag1.toArray()) + "\n" + "Bag 2: "
+                + Arrays.toString(testBag2.toArray()));
+        testBag3 = testBag1.difference(testBag2);
+        System.out
+                .println("Difference Case 4 - when both bags are null: " + Arrays.toString(testBag3.toArray()) + "\n");
 
         // difference method: (case 5) when all elements match
         testBag1.add("c");
@@ -112,32 +143,48 @@ public class LinkedBagTest {
         testBag2.add("e");
         testBag2.add("f");
 
-        System.out.println("Bag 1: " + testBag1 + "\n" + "Bag 2: " + testBag2);
-        System.out.println("Difference Case 4 - when all elements match: " + testBag1.difference(testBag2));
+        System.out.println("Bag 1: " + Arrays.toString(testBag1.toArray()) + "\n" + "Bag 2: "
+                + Arrays.toString(testBag2.toArray()));
+        testBag3 = testBag1.difference(testBag2);
+        System.out
+                .println("Difference Case 4 - when all elements match: " + Arrays.toString(testBag3.toArray()) + "\n");
 
         // UNION METHOD TESTING
         System.out.println("---------- UNION METHOD TESTING ----------");
 
         // union method: (case 1) when they don't have same elements
-        System.out.println("Bag 1: " + testBag1 + "\n" + "Bag 2: " + testBag2);
+        System.out.println("Bag 1: " + Arrays.toString(testBag1.toArray()) + "\n" + "Bag 2: "
+                + Arrays.toString(testBag2.toArray()));
+        testBag3 = testBag1.union(testBag2);
         System.out.println(
-                "Difference Case 1 - when they don't have any matching elements: " + testBag1.union(testBag2));
+                "Difference Case 1 - when they don't have any matching elements: "
+                        + Arrays.toString(testBag3.toArray()) + "\n");
 
         // union method: (case 2) when they have same elements
         testBag1.add("o");
-        System.out.println("Bag 1: " + testBag1 + "\n" + "Bag 2: " + testBag2);
+        System.out.println("Bag 1: " + Arrays.toString(testBag1.toArray()) + "\n" + "Bag 2: "
+                + Arrays.toString(testBag2.toArray()));
+        testBag3 = testBag1.union(testBag2);
         System.out
-                .println("Difference Case 2 - when they have matching elements: " + testBag1.union(testBag2));
+                .println(
+                        "Difference Case 2 - when they have matching elements: " + Arrays.toString(testBag3.toArray())
+                                + "\n");
 
         // union method: (case 3) when one is null
         testBag1.clear();
-        System.out.println("Bag 1: " + testBag1 + "\n" + "Bag 2: " + testBag2);
-        System.out.println("Difference Case 3 - when one of the bags are null: " + testBag1.union(testBag2));
+        System.out.println("Bag 1: " + Arrays.toString(testBag1.toArray()) + "\n" + "Bag 2: "
+                + Arrays.toString(testBag2.toArray()));
+        testBag3 = testBag1.union(testBag2);
+        System.out.println(
+                "Difference Case 3 - when one of the bags are null: " + Arrays.toString(testBag3.toArray()) + "\n");
 
         // union method: (case 4) when both are null
         testBag2.clear();
-        System.out.println("Bag 1: " + testBag1 + "\n" + "Bag 2: " + testBag2);
-        System.out.println("Difference Case 4 - when both bags are null: " + testBag1.union(testBag2));
+        System.out.println("Bag 1: " + Arrays.toString(testBag1.toArray()) + "\n" + "Bag 2: "
+                + Arrays.toString(testBag2.toArray()));
+        testBag3 = testBag1.union(testBag2);
+        System.out
+                .println("Difference Case 4 - when both bags are null: " + Arrays.toString(testBag3.toArray()) + "\n");
 
         // union method: (case 5) when all elements match
         testBag1.add("c");
@@ -150,8 +197,11 @@ public class LinkedBagTest {
         testBag2.add("e");
         testBag2.add("f");
 
-        System.out.println("Bag 1: " + testBag1 + "\n" + "Bag 2: " + testBag2);
-        System.out.println("Difference Case 4 - when all elements match: " + testBag1.union(testBag2));
+        System.out.println("Bag 1: " + Arrays.toString(testBag1.toArray()) + "\n" + "Bag 2: "
+                + Arrays.toString(testBag2.toArray()));
+        testBag3 = testBag1.union(testBag2);
+        System.out
+                .println("Difference Case 4 - when all elements match: " + Arrays.toString(testBag3.toArray()) + "\n");
 
     }
 }
